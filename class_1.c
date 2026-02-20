bab321df53ca75edd28114f2aed638aa83a46e27
Ravi Ranjan <raviranjan9601@gmail.com>
2026-02-20 22:15:53 +0530

    first commit

---
+++ class_1.c
@@ -0,0 +1,12 @@
+//write a program to cluclate area of squre.
+#include <stdio.h>
+
+int main(){
+    int side;
+    printf("Enter a side of squire: ");
+    scanf("%d", &side);
+    
+    printf("area of squre is : %d", side*side);
+    
+    return 0;
+}

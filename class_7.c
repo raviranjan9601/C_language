2280c9017bd1b7d99c5207743e9a0fe676f859a4
Ravi Ranjan <raviranjan9601@gmail.com>
2026-02-21 08:43:49 +0530

    Add arithmetic operator program for addition of two numbers

---
+++ class_7.c
@@ -0,0 +1,19 @@
+//arithmatic operator
+
+#include <stdio.h>
+
+int main(){
+    float num_1, num_2;
+    
+    printf("Enter a number: ");
+    scanf("%f",&num_1);
+    
+    printf("Enter a number: ");
+    scanf("%f",&num_2);
+    
+    float sum = num_1 + num_2;
+    
+    printf("SUM of Your Number is : %f", sum);
+    
+    return 0;
+}
